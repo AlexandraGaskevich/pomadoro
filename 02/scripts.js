c319381b2ae2046a7c66b548c7dd3721d926fe71
AlexandraGaskevich <31881579+AlexandraGaskevich@users.noreply.github.com>
2019-04-13 16:57:32 +0300

    Add files via upload

---
+++ 02/scripts.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('form')
+
+form.onsubmit = function startTimer(e) {
+  event.preventDefault()
+  const input = e.target.querySelector('input[type=text]')
+  console.log(input.value)
+  input.value = ''
+}
